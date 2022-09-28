ced93a9b00af8fc27da8258a602d4f0902188a62
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2022-09-28 16:33:30 -0300

    chore: req 01

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,23 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        for _ in range(15):
+            response = requests.get(
+                url,
+                timeout=3,
+                headers={"user-agent": "Fake user-agent"}
+            )
+            time.sleep(1)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
