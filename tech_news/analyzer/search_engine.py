846f72d4573b0746a1e6ade8d875669680597b8b
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2022-10-04 15:47:17 -0300

    fix: req 08, chore: req 09

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -20,7 +20,7 @@ def search_by_date(date):
 # Requisito 8
 def search_by_tag(tag):
     news_list = search_news(
-        {"tag": {"$elemMatch": {"$regex": tag, "$options": "i"}}}
+        {"tags": {"$elemMatch": {"$regex": tag, "$options": "i"}}}
     )
     result = news_list_result(news_list)
     return result
@@ -28,4 +28,8 @@ def search_by_tag(tag):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news_list = search_news(
+        {"category": {"$regex": category, "$options": "i"}}
+    )
+    result = news_list_result(news_list)
+    return result
